c40406f56aeae40490e3bedef0fca8435c3a762c
Elliott Indiran <eindiran@users.noreply.github.com>
2017-11-01 06:51:28 +0000

    Update apt_prepare.sh

--- sys_tools/apt_prepare.sh
+++ sys_tools/apt_prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Prepare a new system by installing everything I regularly use onto it
 # Intended for Ubuntu 14.04+
+# Attempted on Raspbian/Debian as well
 
 echo "Doing pre-install upgrades"
 sudo apt-get -y update
@@ -8,12 +9,20 @@ sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
 echo "Installing all packages now"
+sudo apt install -y zsh
 sudo apt install -y htop terminator tree shellcheck vim git lynx irssi gawk sox zip unzip pandoc alien rpm
 sudo apt install -y xclip xz-utils audacity lame flac thunderbird chromium-browser
 sudo apt install -y default-jdk gradle groovy junit # Java
 sudo apt install -y rustc cargo rust-gdb # Rust
 sudo apt install -y python3 python3-pip pypy pylint pylint3 pydf # Python
 sudo apt install -y sqlite sqlite3 postgresql-* # Databases
+
+# Do pip based installations
+sudo -H pip3 install thefuck coconut django
+
+# cargo based installations
+cargo install fd-find
+cargo install ripgrep
 
 echo "Cleaning Up" &&
     sudo apt-get -f install &&
